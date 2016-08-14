5157cc910234ff165914d9e5fe1301bec650253d
paulhoughton <paulhoughton@users.noreply.github.com>
2016-08-14 08:08:44 +0100

    Typescript 2

--- src/locationService.ts
+++ src/locationService.ts
@@ -1,7 +1,10 @@
+import * as angular from 'angular';
+
 export default class {
   static $inject = ["$http", "$q"];
-  constructor(private $http: ng.IHttpService, private $q: ng.IQService) { }
-  getLatLon(location: string): ng.IPromise<GoogleGeocodeGeometry[]> {
+  constructor(private $http: angular.IHttpService, private $q: angular.IQService) { }
+
+  getLatLon(location: string): angular.IPromise<GoogleGeocodeGeometry[]> {
     return this.$q((resolve, reject) => {
       this.$http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}`)
         .then((res: {data: any}) => resolve(res.data.results))
